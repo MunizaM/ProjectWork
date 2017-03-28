94920ff7b24faff94712701535848cd6d9969b18
p15209587 <p15209587@GWHXX581WIN015W>
2017-03-28 16:24:30 +0100

    Creation of clsPayment + test class

---
+++ TestFramework/tstPayment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoeWars.Properties;
+
+namespace TestFramework
+{
+    [TestClass]
+    public class tstPayment
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of our class clsPayment
+            clsPayment APayment = new clsPayment();
+            //check to see that the class is not null
+            Assert.IsNotNull(APayment);
+        }
+    }
+}
